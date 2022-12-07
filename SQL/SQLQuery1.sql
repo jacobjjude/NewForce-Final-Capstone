f1b2cdb9ff5e5b9877da5e0f4c97368f2c7153ff
Jacob Jude <jacobj.jude@outlook.com>
2022-12-07 13:57:28 -0500

    initial commit

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,130 @@
+﻿USE [master]
+
+IF db_id('YourPlace') IS NULL
+	CREATE DATABASE [YourPlace]
+GO
+
+USE [YourPlace]
+GO
+
+DROP TABLE IF EXISTS [User];
+DROP TABLE IF EXISTS [Friends];
+DROP TABLE IF EXISTS [Bulletins];
+DROP TABLE IF EXISTS [Status];
+DROP TABLE IF EXISTS [BulletinComments];
+DROP TABLE IF EXISTS [StatusComments];
+DROP TABLE IF EXISTS [Top8Friends];
+DROP TABLE IF EXISTS [Ranking];
+DROP TABLE IF EXISTS [Messages];
+DROP TABLE IF EXISTS [userProfilePage];
+
+
+CREATE TABLE [User] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [firstName] nvarchar(255),
+  [lastName] nvarchar(255),
+  [photoUrl] nvarchar(255),
+  [email] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Friends] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [userProfileIdSender] int,
+  [userProfileIdReceive] int
+)
+GO
+
+CREATE TABLE [Bulletins] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [userProfileId] int,
+  [subject] nvarchar(255),
+  [content] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Status] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [userProfileId] int,
+  [content] nvarchar(255)
+)
+GO
+
+CREATE TABLE [BulletinComments] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [postId] int,
+  [userProfileId] int,
+  [content] nvarchar(255)
+)
+GO
+
+CREATE TABLE [StatusComments] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [statusId] int,
+  [userProfileId] int,
+  [content] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Top8Friends] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [FriendId] int,
+  [RankingId] int,
+  [userId] int
+)
+GO
+
+CREATE TABLE [Ranking] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [rank] int
+)
+GO
+
+CREATE TABLE [Messages] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [userId] int,
+  [friendId] int,
+  [Subject] nvarchar(255),
+  [content] nvarchar(255)
+)
+GO
+
+
+
+ALTER TABLE [Bulletins] ADD FOREIGN KEY ([userProfileId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Status] ADD FOREIGN KEY ([userProfileId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Friends] ADD FOREIGN KEY ([userProfileIdReceive]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [BulletinComments] ADD FOREIGN KEY ([postId]) REFERENCES [Bulletins] ([Id])
+GO
+
+ALTER TABLE [BulletinComments] ADD FOREIGN KEY ([userProfileId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Friends] ADD FOREIGN KEY ([userProfileIdSender]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [StatusComments] ADD FOREIGN KEY ([userProfileId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [StatusComments] ADD FOREIGN KEY ([statusId]) REFERENCES [Status] ([Id])
+GO
+
+ALTER TABLE [Top8Friends] ADD FOREIGN KEY ([RankingId]) REFERENCES [Ranking] ([Id])
+GO
+
+ALTER TABLE [Top8Friends] ADD FOREIGN KEY ([FriendId]) REFERENCES [Friends] ([Id])
+GO
+
+ALTER TABLE [Messages] ADD FOREIGN KEY ([userId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Messages] ADD FOREIGN KEY ([friendId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Top8Friends] ADD FOREIGN KEY ([userId]) REFERENCES [User] ([Id])
---
+++ SQL/SQLQuery2.sql
@@ -0,0 +1,68 @@
+﻿USE [YourPlace]
+GO
+
+set identity_insert [User] on
+insert into [User] ([Id], [firstName], [lastName], [photoUrl], [email]) 
+VALUES (1, 'Ad', 'Min', 'https://iso.500px.com/wp-content/uploads/2015/03/business_cover.jpeg', 'admin@example.com'),
+(2, 'Foo', 'Bar', 'https://www.pexels.com/photo/man-in-brown-coat-and-gray-backpack-posing-for-a-photo-837358/', 'foo@bar.com'),
+(3, 'Jacob', 'Jude', 'https://www.shutterstock.com/image-photo/smiling-young-middle-eastern-man-digital-2063524544', 'jacob@jude.com'),
+(4, 'Mary', 'Smith', 'https://picsum.photos/200', 'eve@smith.com'),
+(5, 'Christian', 'Pulisic', 'https://picsum.photos/200', 'captain@america.com'),
+(6, 'Clifford', 'Thedog', 'https://picsum.photos/200', 'big@red.com'),
+(7, 'Frank', 'Reynolds', 'https://picsum.photos/200', 'paddys@pub.com'),
+(8, 'Walter', 'White', 'https://picsum.photos/200', 'thecook@theonewhoknocks.com'),
+(9, 'Robert', 'Brunner', 'https://picsum.photos/200', 'anarchist@owl.com');
+set identity_insert [User] off
+
+set identity_insert [Friends] on
+insert into [Friends] ([Id], [userProfileIdSender], [userProfileIdReceive])
+VALUES (1, 1, 2), (2, 2, 3), (3,1,3), (4,1,4),(5,1,5),(6,1,6),(7,1,7),(8,1,8),(9,1,9);
+set identity_insert [Friends] off
+
+set identity_insert [Bulletins] on
+insert into [Bulletins] ([Id], [userProfileId], [subject], [content])
+VALUES (1, 1, 'I really love dogs', 'I just really really really love dogs, you know?'),
+(2, 2, 'You know what I am really tired of?', 'People who are unable to put their carts back in the correct spot after grocery shopping'),
+(3, 2, 'Open or have bad luck for 24 hrs', 'Repost or your mom will stop loving you'),
+(4, 3, 'PaRtY aT mY pLaCe', '1234 Nowhere Rd, Huntington, WV');
+set identity_insert [Bulletins] off
+
+set identity_insert [Status] on
+insert into [Status] ([Id], [userProfileId], [content])
+VALUES (1,1,'Hey I really love this app!'),
+(2,1,'Is anyone else having as much fun as I am??'),
+(3,2, 'A new social media site? Lame.'),
+(4,2, 'Someone should buy this site and run it into the ground'),
+(5,3, 'Hey... I am trying my best');
+set identity_insert [Status] off
+
+set identity_insert [BulletinComments] on
+insert into [BulletinComments] ([Id], [postId], [userProfileId], [content])
+VALUES (1, 1, 2, 'What a lame post. No one cares'), 
+(2, 1, 3, 'I also love dogs!'),
+(3, 2, 1, 'Yeah! Those people suck!'),
+(4, 2, 3, 'Those people should not be allowed to grocery shop!'),
+(5, 3, 1, 'NOOO! I fell for it again!');
+set identity_insert [BulletinComments] off
+
+set identity_insert [StatusComments] on
+insert into [StatusComments] ([Id], [statusId], [userProfileId], [content])
+VALUES (1,1,2, 'Hey! Me too!'),
+(2,1,3, 'Thank you!'),
+(3,1,1, 'I am also having fun!');
+set identity_insert [StatusComments] off
+
+set identity_insert [Ranking] on
+insert into [Ranking] ([Id], [rank])
+VALUES (1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8);
+set identity_insert [Ranking] off
+
+set identity_insert [Top8Friends] on
+insert into [Top8Friends] ([Id], [FriendId], [RankingId], [userId])
+VALUES (1, 1, 1, 1), (2, 3, 2, 1), (3, 4, 3, 1), (4, 5, 4, 1), (5,6,5,1), (6,7,6,1), (7,8,7,1), (8,9,8,1);
+set identity_insert [Top8Friends] off
+
+set identity_insert [Messages] on
+insert into [Messages] ([Id], [userId], [friendId], [Subject], [content])
+VALUES (1, 1, 2, 'Test message', 'I just wanna see if these messages work');
+set identity_insert [Messages] off
